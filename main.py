5a98b30bee63cd91e95a559144849da2ff82f9f6
crisviquez <cristopherviquezquiros@gmail.com>
2026-06-09 20:07:55 -0600

    Refactor game loop to include ResultadosEscena
    Added ResultadosEscena for handling game results.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import sys
 from configuracion import *
 from escenas.menu import MenuEscena
 from escenas.nivel1 import Nivel1Escena
+from escenas.resultados import ResultadosEscena
 
 # == Inicializacion ==
 pygame.init()
-FONT = pygame.font.SysFont(FONT_NAME, FONT_SIZE)
+FONT_GRANDE  = pygame.font.SysFont(FONT_NAME, BIG_FONT)   # tiempo, puntos, nombre nivel
+FONT_MEDIANA = pygame.font.SysFont(FONT_NAME, MEDIUM_FONT)   # nombre receta en tarjeta
+FONT_PEQUENA = pygame.font.SysFont(FONT_NAME, SMALL_FONT)   # ingredientes en tarjeta
 
 # == Configuracion ==
 pantalla = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -34,11 +37,15 @@ while ejecutando:
     
     # == Modulo de Logica y Dibujo ==
     escena_actual.actualizar()
-    escena_actual.dibujar(pantalla, FONT)
+    escena_actual.dibujar(pantalla, FONT_GRANDE, FONT_MEDIANA, FONT_PEQUENA)
 
     if escena_actual.siguiente_estado == 'Jugando':
         escena_actual = Nivel1Escena()
-    if escena_actual.siguiente_estado == 'Menu':
+    elif escena_actual.siguiente_estado == 'Nivel1':
+        escena_actual = ResultadosEscena(escena_actual.cocina.puntos)
+    #elif escena_actual.siguente_estado == 'Nivel2':
+        #escena_actual = Nivel2Escena()
+    elif escena_actual.siguiente_estado == 'Menu':
         escena_actual = MenuEscena()
 
     
